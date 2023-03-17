30207953ccd5f458bd4770d77da2c9bc72e6b16d
Kasbolat Kumakhov <uhfath@gmail.com>
2023-03-17 12:53:06 +0300

    Добавлен VerticalAlignment для MudBlazor

---
+++ src/MudBlazor/Enums/VerticalAlignment.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace MudBlazor
+{
+    public enum VerticalAlignment
+	{
+		[Description("baseline")]
+		Base,
+
+		[Description("middle")]
+		Center,
+
+		[Description("top")]
+		Top,
+
+		[Description("bottom")]
+		Bottom,
+	}
+}
