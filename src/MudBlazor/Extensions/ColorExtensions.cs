96e2b4cf8cdb4d6ae5c3ccf0c9e8e2d246a99b9c
Kasbolat Kumakhov <uhfath@gmail.com>
2023-04-27 16:33:05 +0300

    Добавлен метод для формирования цвета CSS по значению Color

---
+++ src/MudBlazor/Extensions/ColorExtensions.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudBlazor.Extensions
+{
+    public static class ColorExtensions
+    {
+        public static string ToCssVarColor(this Color color) =>
+            $"var(--mud-palette-{color.ToDescriptionString()})";
+    }
+}
--- src/MudBlazor/Extensions/EnumExtensions.cs
+++ src/MudBlazor/Extensions/EnumExtensions.cs
@@ -8,22 +8,6 @@ namespace MudBlazor.Extensions
 #nullable enable
     public static class EnumExtensions
     {
-        [Obsolete("Please use the auto-generated ToDescriptionString method instead or implement your own extension method.")]
-        public static string ToDescriptionString(this Enum value)
-        {
-            var field = value.GetType().GetField(value.ToString());
-            if (field is null)
-            {
-                return value.ToString().ToLower();
-            }
-
-            var attributes = Attribute.GetCustomAttributes(field, typeof(DescriptionAttribute), false) as DescriptionAttribute[];
-
-            return attributes is { Length: > 0 }
-                ? attributes[0].Description
-                : value.ToString().ToLower();
-        }
-
         /// <summary>
         /// Universal method that retrieves an array of the values of the constant in specified enumeration, works with nullable and non-nullable enums.
         /// Original <see cref="Enum.GetValues"/> works only with non-nullable enums and will throw exception.
