524038aa30b91dbb095230189581138e198f77f0
Kasbolat Kumakhov <uhfath@gmail.com>
2023-07-05 10:41:51 +0300

    Добавлены новые классы размеров для иконок

--- src/MudBlazor/Enums/Size.cs
+++ src/MudBlazor/Enums/Size.cs
@@ -4,6 +4,10 @@ namespace MudBlazor
 {
     public enum Size
     {
+        [Description("tiny")]
+        Tiny,
+        [Description("little")]
+        Little,
         [Description("small")]
         Small,
         [Description("medium")]
