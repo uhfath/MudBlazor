6ebe1b6a18f949f5c63e30127b70c4a076fc1ca8
Kasbolat Kumakhov <uhfath@gmail.com>
2023-07-13 15:45:08 +0300

    Рефактор

--- src/MudBlazor/Components/Pagination/MudPagination.razor.cs
+++ src/MudBlazor/Components/Pagination/MudPagination.razor.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using MudBlazor.Utilities;
 
